ab280c2d1ddafa12d7f3d84585a11ef195d00bf6
07rashmika <rashmikadesilva07@gmail.com>
2025-09-09 01:04:51 +0530

    feat: implement user authentication with JWT and update user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,33 @@
-//generating api's for user
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "User is not authorized or token is missing" });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "User is not authorized" });
+      }
+      
+      req.user = decoded.user;
+      console.log(req.user);
+      next();
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "User is not authorized or token is missing" });
+  }
+});
+
+module.exports = validateToken;
