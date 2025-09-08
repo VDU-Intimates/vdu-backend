d1fef0e9181fe5e6562c4bc5c6ff229f3b0eb4c2
Yathavan <yathavanyathu27@gmail.com>
2025-09-08 22:06:07 +0530

    login registration function

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user-route");
 
 dotenv.config(); // must be before using process.env
 
 const app = express();
+app.use(express.json());
 const port = 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
@@ -18,3 +16,6 @@ app.listen(port, () => {
 mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log("✅ MongoDB connected"))
         .catch((err) => console.error("❌ MongoDB error:", err));
+
+        
+app.use("/api/auth", userRoutes);
