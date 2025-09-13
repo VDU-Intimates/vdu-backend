add45ced7cb8c6ab90b4202e8dd6b5623e0154db
Minsadi <minsadividathna@gmail.com>
2025-09-13 14:56:24 +0530

    feat: add discount and delivery fee to order invoice response

--- routes/user-route.js
+++ routes/user-route.js
@@ -17,7 +17,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 // Me (protected)
-router.get("/me", validateToken, getUser);
+router.get("/get-user", validateToken, getUser);
 router.patch("/me", validateToken, updateUser);
 
 module.exports = router;
